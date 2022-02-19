c2ad72b225e9dd94ff1c53c9f4ca0b0f0610388d
navdeep94 <navdeepsingh3094@gmail.com>
2022-02-18 22:32:13 -0800

    Added Test Case for the package

--- tests/testthat/test-git_stats.R
+++ tests/testthat/test-git_stats.R
@@ -1,4 +1,24 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
+#test_that("multiplication works", {
+#  expect_equal(2 * 2, 4)
+#})
 
+base_url <- "https://api.github.com/repos/"
+repo_name<-'numpy/numpy'
+params <- gsub(" ", "", paste(repo_name,"/stats/contributors"))
+url <- gsub(" ", "", paste(base_url,params))
+res = httr::GET(url)
+if (res[['status_code']] != 200) {
+  stop()}
+flush.console()
+data = jsonlite::fromJSON(rawToChar(res$content))
+loginname<-'mattip'
+author<-data$author
+if ((loginname %in%  author$login) != TRUE) {
+  stop("This user login name doesn't exist.")}
+index_data<-which(author$login == loginname)
+data_weeks<-data$weeks
+current_user<-data_weeks[[index_data]]
+current_user$w<-anytime::anydate(current_user$w)
+test_that('data types correct', {
+  expect_is(commits_per_user(current_user),'data.frame')
+})
